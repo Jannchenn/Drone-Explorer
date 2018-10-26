ebc924476f89adfe2c6e81fc871d0b5a584bc26b
jannchenn <chenanyi1997@yahoo.com>
2018-10-26 13:57:56 -0700

    drone logic

--- mission/DroneExplore.sh
+++ mission/DroneExplore.sh
@@ -33,12 +33,12 @@ mv *_* roomba
 python Edit.py
 
 # random experiment
-filename="lambdas1.txt"
+filename="indep_var.txt"
 while read -r line
 do
     para="$line"
-    lambdas=(${para})
-    echo ${lambdas[0]} ${lambdas[1]} >| boardinput.txt
+    indep_var=(${para})
+    echo ${indep_var[0]} ${indep_var[1]} >| boardinput.txt
     counter2=1
     while [ ${counter2} -le 2 ]
     do
@@ -46,7 +46,7 @@ do
         ((counter2++))
     done
     # Move generated reports in to new folder
-    folder="${lambdas[0]}_${lambdas[1]}"
+    folder="${indep_var[0]}_${indep_var[1]}"
     mkdir ${folder}
     mv Drone_* ${folder}
     mv Board_* ${folder}
@@ -56,7 +56,7 @@ done < "$filename"
 mkdir random
 mv *_* random
 
-# move random, roomba to fix_buffer/fix_duration
-mkdir fix_buffer #may change
-mv random fix_buffer
-mv roomba fix_buffer
+## move random, roomba to fix_buffer/fix_duration
+#mkdir fix_buffer #may change
+#mv random fix_buffer
+#mv roomba fix_buffer
