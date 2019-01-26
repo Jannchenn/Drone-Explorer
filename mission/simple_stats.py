ef81572ff36015ae8b8705f07ae0a5e417964cff
YuqiYuqiMa <yuqim1@uci.edu>
2019-01-25 20:35:43 -0800

    Update simple_stats.py

--- mission/simple_stats.py
+++ mission/simple_stats.py
@@ -2,9 +2,7 @@ from time import gmtime, strftime, time
 import Board
 from Event import EventMove, EventFix
 
-def stats(info, delimiter=","):
+def stats(total_events, total_caught_events):
     file_name = "catch_rate.txt"
     report = open(file_name, "a")
-    total_event = info[-3]
-    caught_event = info[-2]
-    report.write(str(caught_event / total_event) + '\n')
+    report.write(str(total_events/total_caught_events) + '\n')
